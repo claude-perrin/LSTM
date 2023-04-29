1782b8dbbda39521f0e3337abf7d67b5aa3c1404
claude-perrin <vity5.diduk@gmail.com>
2023-04-29 21:36:02 +0200

    redesigning the model

--- lstm/lstm.py
+++ lstm/lstm.py
@@ -1,158 +1,135 @@
 #import pandas as pd
 #import matplotlib.pyplot as plt
-from activation_functions import tanh_activation, sigmoid
-
+from activation_functions import tanh_activation, sigmoid, softmax
+import numpy as np
 
 class LSTM:
-    def __init__(self):
+    def __init__(self, hidden_size, vocab_size):
         self.ltm = 0
         self.stm = 0
 
-        # Long-term to remember Forget Gate
-        self.w_forget_gate = { "w_stm": 2.7, "w_in": 1.63, "bias": 1.62 }
+        z_size = hidden_size + vocab_size
 
-        # Poterntial Memory to remember ltm InputGate
-        self.w_input_gate_sigmoid = { "w_stm": 2, "w_in": 1.65, "bias": 0.62 } 
-        # Potential Long-Term memory InputGate
-        self.w_input_gate_tan = { "w_stm": 1.41, "w_in": 0.94, "bias": -0.32 }
-
-        # Potential memory to remember stm OutputGate
-        self.w_output_gate = { "w_stm": 4.38, "w_in": -0.19, "bias": 0.59 }
-
-    def forget_gate(self, input):
-        w_stm, w_in, bias = self.w_forget_gate["w_stm"], self.w_forget_gate["w_in"], self.w_forget_gate["bias"]
-        forget_percentage = sigmoid(self.stm * w_stm + input * w_in + bias)
-        return forget_percentage
-
-    def input_gate(self,input):
-        w_stm_sig, w_in_sig, bias_sig = self.w_input_gate_sigmoid["w_stm"], self.w_input_gate_sigmoid["w_in"], self.w_input_gate_sigmoid["bias"]
-        w_stm_tan, w_in_tan, bias_tan = self.w_input_gate_tan["w_stm"], self.w_input_gate_tan["w_in"], self.w_input_gate_tan["bias"]
-
-        percentage_ltm = sigmoid(self.stm * w_stm_sig + input * w_in_sig + bias_sig)
-
-        potential_ltm = tanh_activation(self.stm * w_stm_tan + input * w_in_tan + bias_tan)
-
-        return percentage_ltm, potential_ltm
-
-    def output_gate(self,input): 
-        w_stm, w_in, bias = self.w_output_gate["w_stm"], self.w_output_gate["w_in"], self.w_output_gate["bias"]
-
-        output_gate_out = sigmoid(self.stm * w_stm + input * w_in + bias)
-        return output_gate_out
-
-    def next(self, input):
-        forget_gate_out = self.forget_gate(input)
-
-        percentage_ltm, potential_ltm = self.input_gate(input)
-        input_gate_out = percentage_ltm * potential_ltm
-
-        output_gate_out = self.output_gate(input)
-
-        self.ltm = self.ltm * forget_gate_out + input_gate_out
-        self.stm = output_gate_out * tanh_activation(self.ltm)
-        return (round(self.ltm, 1), round(self.stm,1))
-
-
-    def backpropagation(self, orig_val, pred_val, ltm, stm, output_gate_out,forget_gate_out, input, i, g):
         """
-        orig_val is the one that is pointed by supervised learning
-        pred_val is the value predicted by the lstm 
-        ct = self.ltm
-        ht = self.scm
-        o = output_gate_out
-        i = input_gate_out sigmoid
-        g = input_gate_out tanh
-        f = forget_gate_out
-        x = input
+        Initializes our LSTM network.
+
+        Args:
+            `hidden_size`: the dimensions of the hidden state
+            `vocab_size`: the dimensions of our vocabulary
+            `z_size`: the dimensions of the concatenated input 
+        """
+        # Weight matrix (forget gate)
+
+        self.W_f = np.zeros((z_size, 1))
+
+        # Bias for forget gate
+        self.b_f = np.zeros((hidden_size, 1))
+
+        # Weight matrix (input gate)
+
+        self.W_i = np.zeros((z_size, 1))
+
+        # Bias for input gate
+        self.b_i = np.zeros((hidden_size, 1))
+
+        # Weight matrix (candidate)
+
+        self.W_g = np.zeros((hidden_size, 1))
+
+        # Bias for candidate
+        self.b_g = np.zeros((hidden_size, 1))
+
+        # Weight matrix of the output gate
+        #TODO Not sure
+        self.W_o = np.zeros((hidden_size, 1))
+
+        # Bias for output gate
+        self.b_o = np.zeros((hidden_size, 1))
+
+        # Weight matrix relating the hidden-state to the output
+
+        self.W_v = np.zeros((vocab_size, 1))
+
+        # Bias for logits
+        self.b_v = np.zeros((vocab_size, 1))
+
+        self.W_f = self.init_orthogonal(self.W_f)
+        self.W_i = self.init_orthogonal(self.W_i)
+        self.W_g = self.init_orthogonal(self.W_g)
+        self.W_o = self.init_orthogonal(self.W_o)
+        self.W_v = self.init_orthogonal(self.W_v)
+
+    def init_orthogonal(self, weight):
+        ndim_x, ndim_y = weight.shape
+        W = np.random.randn(ndim_x, ndim_y)
+        u, s, v = np.linalg.svd(W)
+        return u.astype('float32')
+
+    def forward(self, inputs, h_prev, C_prev):
+        """
+        Arguments:
+        x -- your input data at timestep "t", numpy array of shape (n_x, m).
+        h_prev -- stm at timestep "t-1", numpy array of shape (n_a, m)
+        C_prev -- ltm at timestep "t-1", numpy array of shape (n_a, m)
+        Returns:
+            z_s, f_s, i_s, g_s, C_s, o_s, h_s, v_s -- lists of size m containing the computations in each forward pass
+        outputs -- prediction at timestep "t", numpy array of shape (n_v, m)
         """
 
-        E_delta = orig_val - pred_val
+        # Save a list of computations for each of the components in the LSTM
+        x_s, z_s, f_s, i_s,  = [], [] ,[], []
+        g_s, C_s, o_s, h_s = [], [] ,[], []
+        v_s, output_s =  [], [] 
 
-        gradients = {}
-        # Gradient with respect to output gate
-        dE_do = E_delta * tanh_activation(ltm)
+        # Append the initial cell and hidden state to their respective lists
+        h_s.append(h_prev)
+        C_s.append(C_prev)
 
+        for x in inputs:
 
-        # Gradient with respect to input gate dE/di, dE/dg
-        dE_di = E_delta * output_gate_out * (1 - tanh_activation(ltm)^2) * g
-        dE_dg = E_delta * output_gate_out * (1 - tanh_activation(ltm)^2) * i
+            # Concatenate input and hidden state
+            z = np.row_stack((h_prev, x))
+            z_s.append(z)
 
-        # Gradient with respect to forget gate
+            # Calculate forget gate
+            # YOUR CODE HERE!
+            f = sigmoid(np.dot(self.W_f, z) + self.b_f) 
+            f_s.append(f)
 
-        dE_df = E_delta * output_gate_out * (1 - tanh_activation(ltm)^2) * self.ltm
+            # Calculate input gate
+            # YOUR CODE HERE!
+            i = sigmoid(np.dot(self.W_i, z) + self.b_i) 
+            i_s.append(i)
 
-        # Gradient with respect to ltm
+            # Calculate candidate
+            g = tanh_activation(np.dot(self.W_g, z) + self.b_g)
+            g_s.append(g)
 
-        gradients["dE_dltm"] = E_delta * output_gate_out * (1 - tanh_activation(ltm)^2)
+            # Calculate memory state
+            # YOUR CODE HERE!
+            C_prev = np.multiply(C_prev, f) + np.multiply(g,i)
+            C_s.append(C_prev)
 
-        # Gradient with respect to self.ltm
+            # Calculate output gate
+            # YOUR CODE HERE!
+            o = sigmoid(np.dot(self.W_o, z) + self.b_o)
+            o_s.append(o)
 
-        gradients["dE_dself_ltm"] = E_delta * output_gate_out * (1 - tanh_activation(ltm)^2) * forget_gate_out
+            # Calculate hidden state
+            h_prev = o * tanh_activation(C_prev)
+            h_s.append(h_prev)
 
-        # Gradient with respect to output gate weights
+            # Calculate logits
+            v = np.dot(self.W_v, h_prev) + self.b_v
+            v_s.append(v)
 
-        gradients["in_output_gate"] = dE_do * output_gate_out * (1 - output_gate_out) * input
-        gradients["stm_output_gate"] = dE_do * output_gate_out * (1 - output_gate_out) * self.stm
-        gradients["bias_output_gate"] = dE_do * output_gate_out * (1 - output_gate_out)
+            # Calculate softmax
+            output = softmax(v)
+            output_s.append(output)
 
-        # Gradient with respect to forget gate weights
-
-        gradients["in_forget_gate"] = dE_df * forget_gate_out * (1 - forget_gate_out) * input
-        gradients["stm_forget_gate"] = dE_df * forget_gate_out * (1 - forget_gate_out) * self.stm
-        gradients["bias_forget_gate"] = dE_df * forget_gate_out * (1 - forget_gate_out)
-
-        # Gradient with respect to input gate weights:
-
-        gradients["in_input_gate_sig"] = dE_di * g * (1 - g) * input
-        gradients["stm_input_gate_sig"] = dE_di * g * (1 - g) * self.stm
-        gradients["bias_input_gate_sig"] = dE_di * g * (1 - g)
-
-
-        gradients["in_input_gate_tan"] = dE_dg * i * (1 - i) * input
-        gradients["stm_input_gate_tan"] = dE_dg * i * (1 - i) * self.stm
-        gradients["bias_input_gate_tan"] = dE_dg * i * (1 - i)
-
-        return gradients
-
-    def update_weights(self, grads, learning_rate):
-        #update output gate weights
-        self.w_output_gate["w_stm"] -= learning_rate * grads["stm_output_gate"] 
-        self.w_output_gate["w_in"] -= learning_rate * grads["in_output_gate"] 
-
-        #update input gate sigmoid weights
-        self.w_input_gate_sigmoid["w_stm"] -= learning_rate * grads["stm_input_gate"] 
-        self.w_input_gate_signoid["w_in"] -= learning_rate * grads["in_input_gate"] 
-
-        #update input gate tanh weights
-        self.w_input_gate_tan["w_stm"] -= learning_rate * grads["stm_input_gate_tan"] 
-        self.w_input_gate_tan["w_in"] -= learning_rate * grads["in_input_gate_tan"] 
+        return z_s, f_s, i_s, g_s, C_s, o_s, h_s, v_s, output_s
 
 
-        #update forget gate weights
-        self.w_forget_gate["w_stm"] -= learning_rate * grads["stm_forget_gate"] 
-        self.w_forget_gate["w_in"] -= learning_rate * grads["in_forget_gate"] 
-
-    
-    def train(self, input_array, target_array):
-        for input in input_array:
-            self.next(input)
-
-
-
-
-
-    def get_weights(self):
-        return {"forget_gate":self.w_forget_gate, "input_sig": self.w_input_gate_sigmoid, "input_tan": self.w_input_gate_tan, "output": self.w_output_gate}
-    
 if __name__ == "__main__":
-    ltm, stm = (0,0)
-    lstm = LSTM()
-    data = [0,0.5,0.25,1]
-    for i in range(len(data)):
-        output = lstm.next(data[i])
-        ltm, stm = output
-        print(output)
-        print("\n\nANOTHER LAYER\n\n")
-print(ltm, stm)
-for key,value in lstm.get_weights().items():
-    print(key, value)
+    lstm = LSTM(1,1)
+    print(lstm.W_f)
