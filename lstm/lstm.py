7afeac41d51cb68be12e35453f675b1b2c31e974
claude-perrin <vity5.diduk@gmail.com>
2023-04-30 12:39:12 +0200

    feedforward

--- lstm/lstm.py
+++ lstm/lstm.py
@@ -2,7 +2,7 @@
 #  import matplotlib.pyplot as plt
 from activation_functions import tanh_activation, sigmoid, softmax
 import numpy as np
-import dataset
+from dataset import Dataset
 
 class LSTM:
     def __init__(self, hidden_size, vocab_size):
@@ -20,9 +20,7 @@ class LSTM:
             `z_size`: the dimensions of the concatenated input 
         """
         # Weight matrix (forget gate)
-
         self.W_f = np.zeros((z_size, 1))
-
         # Bias for forget gate
         self.b_f = np.zeros((hidden_size, 1))
 
@@ -35,36 +33,85 @@ class LSTM:
 
         # Weight matrix (candidate)
 
-        self.W_g = np.zeros((hidden_size, 1))
+        self.W_g = np.zeros((z_size, 1))
 
         # Bias for candidate
         self.b_g = np.zeros((hidden_size, 1))
 
         # Weight matrix of the output gate
         #TODO Not sure
-        self.W_o = np.zeros((hidden_size, 1))
+        self.W_o = np.zeros((z_size, 1))
 
         # Bias for output gate
         self.b_o = np.zeros((hidden_size, 1))
 
         # Weight matrix relating the hidden-state to the output
 
-        self.W_v = np.zeros((vocab_size, 1))
+        self.W_v = np.zeros((hidden_size, 1))
 
         # Bias for logits
         self.b_v = np.zeros((vocab_size, 1))
 
-        self.W_f = self.init_orthogonal(self.W_f)
-        self.W_i = self.init_orthogonal(self.W_i)
-        self.W_g = self.init_orthogonal(self.W_g)
-        self.W_o = self.init_orthogonal(self.W_o)
-        self.W_v = self.init_orthogonal(self.W_v)
+        self.W_f = self.__init_orthogonal(self.W_f)
+        self.W_i = self.__init_orthogonal(self.W_i)
+        self.W_g = self.__init_orthogonal(self.W_g)
+        self.W_o = self.__init_orthogonal(self.W_o)
+        self.W_v = self.__init_orthogonal(self.W_v)
 
-    def init_orthogonal(self, weight):
-        ndim_x, ndim_y = weight.shape
-        W = np.random.randn(ndim_x, ndim_y)
-        u, s, v = np.linalg.svd(W)
-        return u.astype('float32')
+    def __init_orthogonal(self,param):
+        """
+        Initializes weight parameters orthogonally.
+        This is a common initiailization for recurrent neural networks.
+
+        Refer to this paper for an explanation of this initialization:
+            https://arxiv.org/abs/1312.6120
+        """
+        if param.ndim < 2:
+            raise ValueError("Only parameters with 2 or more dimensions are supported.")
+
+        rows, cols = param.shape
+
+        new_param = np.random.randn(rows, cols)
+
+        if rows < cols:
+            new_param = new_param.T
+
+        # Compute QR factorization
+        q, r = np.linalg.qr(new_param)
+
+        # Make Q uniform according to https://arxiv.org/pdf/math-ph/0609050.pdf
+        d = np.diag(r, 0)
+        ph = np.sign(d)
+        q *= ph
+
+        if rows < cols:
+            q = q.T
+
+        new_param = q
+
+        return np.squeeze(new_param)
+
+    def __clip_gradient_norm(grads, max_norm=0.25):
+        """
+        Clips gradients to have a maximum norm of `max_norm`.
+        This is to prevent the exploding gradients problem.
+        """ 
+        # Set the maximum of the norm to be of type float
+        max_norm = float(max_norm)
+        total_norm = 0
+        # Calculate the L2 norm squared for each gradient and add them to the total norm
+        for grad in grads:
+            grad_norm = np.sum(np.power(grad, 2))
+            total_norm += grad_norm
+        total_norm = np.sqrt(total_norm)
+        # Calculate clipping coeficient
+        clip_coef = max_norm / (total_norm + 1e-6)
+        # If the total norm is larger than the maximum allowable norm, then clip the gradient
+        if clip_coef < 1:
+            for grad in grads:
+                grad *= clip_coef
+        return grads
+
 
     def forward(self, inputs, h_prev, C_prev):
         """
@@ -83,13 +130,11 @@ class LSTM:
         # Append the initial cell and hidden state to their respective lists
         forward_pass["h_s"].append(h_prev)
         forward_pass["C_s"].append(C_prev)
-
         for x in inputs:
 
             # Concatenate input and hidden state
-            z = np.row_stack((h_prev, x))
+            z = np.squeeze(np.row_stack((h_prev, x)))
             forward_pass["z_s"].append(z)
-
             # Calculate forget gate
             f = sigmoid(np.dot(self.W_f, z) + self.b_f) 
             forward_pass["f_s"].append(f)
@@ -126,23 +171,26 @@ class LSTM:
 
 
 if __name__ == "__main__":
-    lstm = LSTM(1,1)
-    print(lstm.W_f)
+    dataset = Dataset()
+    dataset.generate_dataset()
+    hidden_size = 50 # Number of dimensions in the hidden state
+    vocab_size  = len(dataset.word_to_idx) # Size of the vocabulary used
+    lstm = LSTM(hidden_size,vocab_size)
     # Get first sentence in test set
-    inputs, targets = test_set[1]
+    inputs, targets = dataset.test_set.inputs[1], dataset.test_set.targets[1]
 
     # One-hot encode input and target sequence
-    inputs_one_hot = one_hot_encode_sequence(inputs, vocab_size)
-    targets_one_hot = one_hot_encode_sequence(targets, vocab_size)
+    inputs_one_hot = dataset.one_hot_encode_sequence(inputs)
+    targets_one_hot = dataset.one_hot_encode_sequence(targets)
 
     # Initialize hidden state as zeros
     h = np.zeros((hidden_size, 1))
     c = np.zeros((hidden_size, 1))
 
     # Forward pass
-    z_s, f_s, i_s, g_s, C_s, o_s, h_s, v_s, outputs = forward(inputs_one_hot, h, c, params)
+    forward_pass = lstm.forward(inputs_one_hot, h, c)
 
-    output_sentence = [idx_to_word[np.argmax(output)] for output in outputs]
+    output_sentence = [dataset.idx_to_word[np.argmax(output)] for output in forward_pass["output_s"]]
     print('Input sentence:')
     print(inputs)
 
@@ -150,5 +198,5 @@ if __name__ == "__main__":
     print(targets)
 
     print('\nPredicted sequence:')
-    print([idx_to_word[np.argmax(output)] for output in outputs])
+    print([dataset.idx_to_word[np.argmax(output)] for output in forward_pass["output_s"]])
 
